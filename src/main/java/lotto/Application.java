3d8732005fc605f0d9f20d0d9a5e248367da7d26
donnyeonglee <dlehssud@snu.ac.kr>
2025-10-30 20:14:12 +0900

    feat: 로또 구입 금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,9 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Input input = new Input();
+
+        long money = InputValidation.validatedMoney(input.enterMoney());
+        System.out.println("구매금액 : " + money); // 테스트 출력
     }
 }
---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    static final String PROMPT_MONEY = "구입금액을 입력해 주세요.";
+
+    public String enterMoney() {
+        System.out.println(PROMPT_MONEY);
+        return Console.readLine();
+    }
+}
