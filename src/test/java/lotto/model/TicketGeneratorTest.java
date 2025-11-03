ab5e8b3049c542ef9f382244ebdb30dbf7c21920
donnyeonglee <dlehssud@snu.ac.kr>
2025-11-03 10:33:59 +0900

    test: 복권 구매 단위 테스트 추가

---
+++ src/test/java/lotto/model/TicketGeneratorTest.java
@@ -0,0 +1,20 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketGeneratorTest {
+    @DisplayName("구입금액이 3000원이면 복권 3개를 구매한다.")
+    @Test
+    void 구입금액이_3000원이면_복권_3개를_구매한다() {
+        TicketGenerator ticketGenerator = new TicketGenerator(3000);
+        assertThat(ticketGenerator.ticketList).contains("3개를 구매했습니다.");
+        assertThat(ticketGenerator.getTickets().size() == 3).isTrue();
+    }
+}
