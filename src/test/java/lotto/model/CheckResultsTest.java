fb25c19c0a6216ce998ae0fc1e801482bbba562f
donnyeonglee <dlehssud@snu.ac.kr>
2025-11-03 11:42:28 +0900

    test: 당첨 결과 확인 및 수익률 계산 단위 테스트 추가

---
+++ src/test/java/lotto/model/CheckResultsTest.java
@@ -0,0 +1,45 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckResultsTest {
+    @DisplayName("구입금액 1000원 2등 당첨 기능 테스트")
+    @Test
+    void 구입금액1000원_2등_당첨_기능_테스트() {
+        long money = 1000;
+        List<Integer> lottoNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<List<Integer>> tickets = new ArrayList<>();
+        tickets.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 7))); // 2등 당첨
+        CheckResults checkResults = new CheckResults(money, lottoNumbers, bonusNumber, tickets);
+        assertThat(String.format("%.1f%%", checkResults.getYield())).isEqualTo("3000000.0%");
+        assertThat(checkResults.getEachRankCount()).isEqualTo(new ArrayList<>(Arrays.asList(0, 1, 0, 0, 0)));
+    }
+
+    @DisplayName("구입금액 5000원 모든 등수 당첨 기능 테스트")
+    @Test
+    void 구입금액6000원_모든_등수_당첨_기능_테스트() {
+        long money = 6000;
+        List<Integer> lottoNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<List<Integer>> tickets = new ArrayList<>();
+        tickets.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6))); // 1등
+        tickets.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 7))); // 2등
+        tickets.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 8))); // 3등
+        tickets.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 7, 8))); // 4등
+        tickets.add(new ArrayList<>(Arrays.asList(1, 2, 3, 8, 9, 10))); // 5등
+        tickets.add(new ArrayList<>(Arrays.asList(11, 12, 13, 14, 15, 16))); // 낙첨
+        CheckResults checkResults = new CheckResults(money, lottoNumbers, bonusNumber, tickets);
+        assertThat(String.format("%.1f%%", checkResults.getYield())).isEqualTo("33859250.0%");
+        assertThat(checkResults.getEachRankCount()).isEqualTo(new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1)));
+    }
+
+}
