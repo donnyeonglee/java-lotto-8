949a31bd0caeeaa97d583f566b8e893baa3d42a7
donnyeonglee <dlehssud@snu.ac.kr>
2025-11-03 10:43:12 +0900

    style: 불필요한 import문 제거

--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -3,10 +3,7 @@ package lotto.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
     @DisplayName("구입 금액이 1000의 배수가 아니면 예외가 발생한다.")
--- src/test/java/lotto/model/TicketGeneratorTest.java
+++ src/test/java/lotto/model/TicketGeneratorTest.java
@@ -3,11 +3,7 @@ package lotto.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class TicketGeneratorTest {
     @DisplayName("구입금액이 3000원이면 복권 3개를 구매한다.")
